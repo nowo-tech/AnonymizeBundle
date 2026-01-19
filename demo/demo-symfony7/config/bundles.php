d4cdeb749a157c38e5bc72d47e76e3f79d95822b
Héctor Franco <hectorfranco@nowo.tech>
2026-01-19 14:04:03 +0100

    chore: Update Symfony 6 and 7 demos to bundle v0.0.6
    - Updated bundle version from v0.0.4 to v0.0.6 in both demos
    - Added AnonymizeBundle to Symfony 7 bundles.php (was missing)
    - All demos now use the latest bundle version

--- demo/demo-symfony7/config/bundles.php
+++ demo/demo-symfony7/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Nowo\AnonymizeBundle\AnonymizeBundle::class => ['dev' => true, 'test' => true],
 ];
